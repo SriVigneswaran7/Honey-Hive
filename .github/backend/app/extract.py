f21778c17df5d976cbc39cbc9d5e9ab967ffe0e0
Ketchup256 <Isaamxbox@gmail.com>
2026-03-03 21:39:35 +0000

    feat:Amazon link analysis

--- .github/backend/app/extract.py
+++ .github/backend/app/extract.py
@@ -4,56 +4,47 @@ from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
 load_dotenv(find_dotenv())
-link="https://www.amazon.co.uk/Bluetooth-Headphones-KVIDIO-Microphone-Lightweight-black/dp/B09PQSVFQT/ref=sr_1_1_sspa?crid=3LQGEWV27N7XJ&dib=eyJ2IjoiMSJ9.NfXLjB13X46Utxfght_6y6QIrGnY4tDou4BfLb5ClsVqg1tJfEhoEo35ALvIKwz-R5iivPuzyzCE1IWX3Ztm5aAGDIG3FybV2kJ1928s_Gb4BeOnhtO5VEyssvj4b8TaNcXqmCkSbMJNrJGCWW5DhQQ4gJjD_H1HwpjcHq4zM2OqJZ1IhHQjiSXR6hgOBuv27FC-ntADDAUZvyjL6Y7FSM1aqC8oJdg5B189AJaffuQ.3MGgAhX-uP-62HQ8rTbYeC0QvBqWzp7ZRxHhy5LM914&dib_tag=se&keywords=headphones&qid=1772305213&sprefix=h%2Caps%2C261&sr=8-1-spons&aref=xjpdC0soXE&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1"
-
-#link analysis
-split_link=link.split("/")
-cleanlink=""
-for i in split_link:
-    if i[0:3]!='ref':
-        cleanlink=cleanlink+'/'+ i
-cleanlink=cleanlink[1:]
-
-
-#classification
-if split_link[2]=="www.amazon.co.uk":
-    item=split_link[3]
-    initialspecs=item.split('-')
-
 api_key = os.getenv("SERPAPI_KEY")
-params = {
-    "engine": "google",
-    "q": f'site:{cleanlink}',
-    "api_key": api_key
-}
-response = requests.get("https://serpapi.com/search", params=params)
-data=response.json()
-organic= data.get("organic_results", [])
-result= organic[0]
-detected = (
-        result
-        .get("rich_snippet", {})
-        .get("bottom", {})
-        .get("detected_extensions", {})
-    )
-price = detected.get("price")
-currency = detected.get("currency")
+link="https://www.amazon.co.uk/Sumvision-Programmable-Performance-Ergonomic-Computer/dp/B08LZJ3FQQ/ref=sr_1_1_sspa?crid=2H7130RAD2NNB&dib=eyJ2IjoiMSJ9.YpZv4TS9bX7IKiZ3wotjjFnM-VrCVs7B2zGjFImKavP7G4QYn6vGZNdI-1aD0sRUDsVKgMK19dcPoLWhNPb9of6fLTferhvwrEGQQHBSywBiH6aBoEJDamUTEWQoBIx36yIrBCPXtcUBsZcdyd4nZSHHv_J5oZqmdhS7EqsidSZCdckDhzu5xzjgiOQtAdVWaP25yDv-Hje_gwXFbZ_MZ_AtARs40qfnaLcTcmJb9Pw8gU9eXhkPL2Txws2YC-2jYB-LGZXs2ay8ZBufyFK2KEMUasKYfuruSdVdyhHSKNk.Brao0uUHJSBE4vvVKAU4xAIgBA894x3-mKBw9SVMNi4&dib_tag=se&keywords=gaming+mouse&qid=1772535126&s=electronics&sprefix=g%2Celectronics%2C345&sr=1-1-spons&aref=I2SMTJ500e&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1"
+split_link=link.split("/")
+def link_analysis_web(split_link):
+    if split_link[2]=="www.amazon.co.uk":
+        site="Amazon"
+        asin=split_link[5]
+        item=split_link[3]
+        initialspecs=item.split('-')
+    return [site,asin,initialspecs]
+def scrape_data(asin):
+    params = {
+        "engine": "amazon_product",
+        "asin": asin,
+        "amazon_domain": "amazon.co.uk",
+        "api_key": api_key
+    }
+    response = requests.get("https://serpapi.com/search", params=params)
+    data = response.json()
+    return data
+def data_extraction(data):
+    product_results = data.get('product_results', {})
+    price = product_results.get('price', 'N/A')
+    extracted_price = product_results.get('extracted_price', 0.0)
+    features = data.get('about_item', [])
+    
+    reviews_info = data.get('reviews_information', {})
+    review_count = product_results.get('reviews', 0)
+    review_summary = reviews_info.get('summary', {}).get('text', 'No summary available.')
+    print(f"PRICE: {price} (Numeric: {extracted_price})")
+    print("\nFEATURES:")
+    for feature in features:
+        print(f"- {feature}")
 
-# features
-snippet = result.get("snippet", "")
-cleaned = snippet.replace(";", ",")
-totalspecs=cleaned.split(",")
-finalspecs=[]
-finalfeats=[]
-for i in totalspecs:
-    if i.strip()!="Product Features":
-        finalspecs.append(i.strip())
-    else:
-        break
-for i in totalspecs:
-    if i.strip() not in finalspecs:
-        finalfeats.append(i.strip())
-print(initialspecs)
-print(currency,price)
-print(finalspecs[1:])
-print(finalfeats[1:])
+    print(f"\nREVIEWS ({review_count} total):")
+    print(f"Summary: {review_summary}")
+    
+
+site,asin,initialspecs=link_analysis_web(split_link)
+if site="Amazon":\
+    print(initialspecs)
+    data=scrape_data(asin)
+    data_extraction(data)
+
