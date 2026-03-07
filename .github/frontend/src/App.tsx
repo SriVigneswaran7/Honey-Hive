d632a0f2c72715e0cd01476cf74e5e3a100f42c1
SriVigneswaran7 <sa7543b@greenwich.ac.uk>
2026-03-07 22:01:45 +0000

    refactor: upgraded app routing and finalised core entry points

--- .github/frontend/src/App.tsx
+++ .github/frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import './index.css';
+import Results from './Results';
+import Details from './Details';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/review" element={<Details />} />
       </Routes>
     </Router>
   );
