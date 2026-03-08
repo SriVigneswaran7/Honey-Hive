8c2de40bb81a1016adee035439fbb744c58c3058
vez767 <wa3960f@greenwich.ac.uk>
2026-03-08 14:44:16 +0000

    resloving merge conflict issues

--- .github/frontend/src/App.tsx
+++ .github/frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
-import Signup from './Signup';
 import './index.css';
 import Results from './Results';
 import Details from './Details';
@@ -13,11 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
-=======
         <Route path="/results" element={<Results />} />
         <Route path="/review" element={<Details />} />
->>>>>>> origin/feat/frontend
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
